8377a50e357026d43b4323a1be101e337b9b7490
Maarten Bron <maarten@roads2ranking.com>
2020-11-23 16:37:59 +0100

    added the routes, new and create for skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,16 +1,36 @@
 class SkillsController < ApplicationController
-  before_action :set_skill_id, only: :show
+  before_action :set_skill_id, only: [:show]
 
   def index
-  @skills = Skill.all
+    @skills = Skill.all
   end
 
   def show
   end
 
+  def new
+    @user = User.find(params[:user_id])
+    @skill = Skill.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @skill = Skill.new(strong_params_skills)
+    @skill.user = @user
+    if @skill.save
+      redirect_to skills_path
+    else
+      render :new
+    end
+  end
+
   private
 
-  def set_skill_id
-    @skill = Skill.find(params[:id])
+  def set_user_id
+    @skill = skill.find(params[:id])
+  end
+
+  def strong_params_skills
+    params.require(:skill).permit(:name, :description, :price_per_day)
   end
 end
