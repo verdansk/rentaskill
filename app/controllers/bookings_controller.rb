bc2073af9ac757762e986e6dfc74b3a55d4cd51d
Tiago Enes <enes.tiago@gmail.com>
2020-11-24 10:07:09 +0100

    update bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,15 @@
 class BookingsController < ApplicationController
+  before_action :set_user
+  before_action :set_skill, only: [:new, :create]
   def show
-    @user =current_user
     @bookings = Booking.where("user_id = ?", @user.id)
   end
 
   def new
-    @user = current_user
-    @skill = Skill.find(params[:skill_id])
     @booking = Booking.new
   end
 
   def create
-    @skill = Skill.find(params[:skill_id])
-    @user = current_user
     @booking = Booking.new(booking_params)
     @booking.user = @user
     @booking.skill = @skill
@@ -25,8 +22,15 @@ class BookingsController < ApplicationController
 
   private
 
+  def set_user
+    @user = current_user
+  end
+
+  def set_skill
+    @skill = Skill.find(params[:skill_id])
+  end
+
   def booking_params
-    params.require(:booking).permit(:date_start,:date_end,:price)
+    params.require(:booking).permit(:date_start, :date_end, :price)
   end
 end
-
