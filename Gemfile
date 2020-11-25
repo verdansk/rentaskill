f8d672542f83d615491897f55efa43ebcc5c35ff
Tiago Enes <enes.tiago@gmail.com>
2020-11-25 21:32:41 +0100

    added activeRecords and cloudinary to add an image to users

--- Gemfile
+++ Gemfile
@@ -65,3 +65,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'faker'
 gem 'devise'
 gem "select2-rails"
+
+gem 'dotenv-rails', groups: [:development, :test]
+
+gem 'cloudinary', '~> 1.16.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :skills
+  has_one_attached :photo
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
